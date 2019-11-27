aacf2858a5b4a763ac001ff0b250fff73774a652
Taeckyung LEE <37440528+terry00123@users.noreply.github.com>
2019-11-27 22:04:04 +0900

    Update exp_main.py

--- exp_main.py
+++ exp_main.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--log_file', default='./log_cgp.txt', help='Log file name')
     parser.add_argument('--log_folder', default='./log_folder', help='Log folder name')
     parser.add_argument('--mode', '-m', default='evolution', help='Mode (evolution / retrain / reevolution)')
-    parser.add_argument('--max_gen', '-max', default=250, help='Num. of max evaluations')
+    parser.add_argument('--max_gen', '-max', type=int, default=250, help='Num. of max evaluations')
     parser.add_argument('--init', '-i', action='store_true')
     parser.add_argument('--snm', '-snm', type=str, default='normal', choices=['normal', 'strong'], help='Strong Neutral Mutation')
     parser.add_argument('--seed', '-s', type=int, default=470, help="Numpy random seed")
