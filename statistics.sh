52eef265ee608258f2b5e2e7e0fc6521b0133f9a
Taeckyung LEE <37440528+terry00123@users.noreply.github.com>
2019-11-28 12:08:20 +0900

    Update statistics.sh

--- statistics.sh
+++ statistics.sh
@@ -3,5 +3,5 @@
 S=$1
 E=$2
 for ((i=S; i<=E; i++)); do
-  python exp_main.py --max_gen 30 --log_folder "./log_${i}" > "output_${i}"
+  python exp_main.py --max_gen 30 --log_folder "./logs_statistics_${i}" > "output_statistics_${i}"
 done
