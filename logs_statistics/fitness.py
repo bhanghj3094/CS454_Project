f13c2b173e3cca00890ee5f9a81cef2cc956b2e4
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-12-09 23:00:31 +0900

    feat(): gathering fitness

---
+++ logs_statistics/fitness.py
@@ -0,0 +1,22 @@
+import csv
+
+# list to write
+total = [[] for _ in range(30)]
+
+for i in range(40):
+  name = "logs_statistics_" + str(i) + "/log_cgp.txt"
+
+  f = open(name, "r")
+  lines = f.readlines()
+  for j in range(len(lines)):
+    line = lines[j] 
+    list = line.split(",")
+    total[j].append(list[3])
+  f.close()
+
+w = open("fitness.csv", 'a')
+writer = csv.writer(w, lineterminator='\n') 
+for i in range(30):
+  writer.writerow(total[i]) 
+w.close()
+
